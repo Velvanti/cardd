667f55d48305530a547bd8bcad4b7370d94ddc47
Velvanti <dartic14@gmail.com>
2025-04-13 05:53:10 -0400

    Second commit, fixing stupid css I hate this

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const audio = document.getElementById('birthdayAudio');
 audio.volume = 0.5;
+
+function playAudio() {
+    const audio = document.getElementById('birthdayAudio');
+    if (audio.paused) {
+        audio.play(); // Play the audio if it's paused
+    } else {
+        audio.pause(); // Pause the audio if it's already playing
+    }
+}
